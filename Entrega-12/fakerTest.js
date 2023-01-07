873149fe10412ee57601017174b24847282c5d30
Emiliano Caballero <emiliano.caballero98@gmail.com>
2023-01-07 17:51:49 -0300

    faker funciona, solo recordar instalar las dependencias de los demas

--- Entrega-12/fakerTest.js
+++ Entrega-12/fakerTest.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import ProductosRouter from './faker/fakeRoutes';
+import express from 'express'
+import ProductosRouter from './faker/router/usuarios.js'
 
 const app = express()
 
